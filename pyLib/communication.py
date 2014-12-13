a6552e663e01128a01aa5d983238e0f492af9fda
Nick Stockton <nstockton@users.noreply.github.com>
2014-12-13 06:24:06 -0800

    Communication script updated to Python 3

--- pyLib/communication.py
+++ pyLib/communication.py
@@ -1,21 +1,28 @@
+import codecs
+
 from .tintin import TinTin
 
-def review(channel, text=""):
+def review(channelName, searchString=""):
 	try:
-		with open("communication/%s.txt"%channel, "rb") as f:
-			data = f.readlines()
+		with codecs.open("communication/{0}.txt".format(channelName), "rb", encoding="utf-8") as channelFile:
+			channelLines = channelFile.readlines()
 	except IOError as e:
-		TinTin.echo("%s: '%s'" % (e.strerror, e.filename), "mume")
-	text = text.strip()
-	if not data:
-		output = ["%s log is empty!" % channel.capitalize()]
-	elif text.isdigit() and int(text)>=1:
-		output = data[-int(text):]
-	elif not text.isdigit() and text!="":
-		output = [line for line in data if text in line.lower()]
+		return TinTin.echo("{0}: '{1}'".format(e.strerror, e.filename), "mume")
+	searchString = searchString.strip()
+	if not channelLines:
+		# The channel log file is empty.
+		output = ["{0} log is empty!".format(channelName.capitalize())]
+	elif searchString.isdigit() and int(searchString) >= 1:
+		# The search string is a number, so output the last (N) lines.
+		output = channelLines[-int(searchString):]
+	elif not searchString.isdigit() and searchString != "":
+		# Output lines that contain the search string.
+		output = [line for line in channelLines if searchString in line.lower()]
 	else:
-		output = data[-20:]
+		# The search string is empty, so output the last 20 lines.
+		output = channelLines[-20:]
 	if not output:
+		# A search string was specified, but no lines matched it.
 		output = ["Nothing found!"]
 	for line in output[-100:]:
 		TinTin.echo(line.strip(), "mume")
