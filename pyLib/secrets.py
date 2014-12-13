2fad2053714c9d2c92c9fd4898039fc828e48f6a
Nick Stockton <nstockton@users.noreply.github.com>
2014-12-13 12:34:47 -0800

    Secret exits script updated to Python 3

--- pyLib/secrets.py
+++ pyLib/secrets.py
@@ -1,149 +1,140 @@
-import os.path
+import codecs
 import json
+import os.path
+
 from .tintin import TinTin
 
 DATABASE_FILE = "data/secret_exits.json"
 SAMPLE_DATABASE_FILE = "data/secret_exits.json.sample"
+VALID_DIRECTIONS = ["north", "south", "east", "west", "up", "down"]
 
-if os.path.exists(DATABASE_FILE):
-	if not os.path.isdir(DATABASE_FILE):
-		path = DATABASE_FILE
-	else:
-		TinTin.echo("Error: '%s' is a directory, not a file." % DATABASE_FILE, "python")
-elif os.path.exists(SAMPLE_DATABASE_FILE):
-	if not os.path.isdir(SAMPLE_DATABASE_FILE):
-		path = SAMPLE_DATABASE_FILE
-	else:
-		TinTin.echo("Error: '%s' is a directory, not a file." % SAMPLE_DATABASE_FILE, "python")
-else:
-	path = None
-	TinTin.echo("Error: neither '%s' nor '%s' can be found." % (DATABASE_FILE, SAMPLE_DATABASE_FILE), "mume")
 
-if not path:
-	rooms = {}
-else:
-	try:
-		with open(path, "rb") as data:
-			rooms = json.load(data, encoding="UTF-8")
-	except IOError as e:
-		rooms = {}
-		TinTin.echo("%s: '%s'" % (e.strerror, e.filename), "mume")
-	except ValueError as e:
-		rooms = {}
-		TinTin.echo("Corrupted file: '%s'" % path, "mume")
-
-validDirections = ["north", "south", "east", "west", "up", "down"]
-roomName = ""
-
-def uniq(lst):
-	last = object()
-	for item in lst:
-		if item != last:
-			yield item
-			last = item
-
-def save():
-	with open(DATABASE_FILE, "wb") as data:
-		json.dump(rooms, data, sort_keys=True, indent=2, separators=(",", ": "), encoding="UTF-8")
-
-def setRoomName(name=""):
-	global roomName
-	roomName = name.strip().lower()
-
-def info(text="", exactMatch=True):
-	global rooms
-	global roomName
-	text = text.strip().lower()
-	results = []
-	if text:
-		results.extend([name for name in rooms if text in name])
-	elif roomName and roomName in rooms:
-		text = roomName
-		results.append(text)
-	else:
-		return TinTin.echo("Error!  Room undefined.", "mume")
-	if not results:
-		TinTin.echo("There aren't any rooms named '%s' in the database." % text, "mume")
-	else:
-		for result in sorted(results):
-			exits = []
-			for door, direction in rooms[result]:
-				if direction:
-					exits.append("%s (%s)" % (direction, door))
-				else:
-					exits.append("(%s)" % door)
-			TinTin.echo("%s: %s" % (result, ", ".join(exits)), "mume")
-
-def actionAll(action=""):
-	global rooms
-	global roomName
-	currentRoom = roomName
-	if not currentRoom:
-		return TinTin.echo("Error!  Current room undefined.", "mume")
-	elif currentRoom not in rooms:
-		return TinTin.echo("There aren't any rooms named '%s' in the database." % currentRoom, "mume")
-	elif not action:
-		return TinTin.echo("Error! You must specify an action to perform.", "mume")
-	for door, direction in rooms[currentRoom]:
-		if direction:
-			TinTin.send("%s %s %s" % (action, door, direction), "mume")
+class Secrets(object):
+	def __init__(self):
+		if os.path.exists(DATABASE_FILE):
+			if not os.path.isdir(DATABASE_FILE):
+				path = DATABASE_FILE
+			else:
+				TinTin.echo("Error: '{0}' is a directory, not a file.".format(DATABASE_FILE), "python")
+		elif os.path.exists(SAMPLE_DATABASE_FILE):
+			if not os.path.isdir(SAMPLE_DATABASE_FILE):
+				path = SAMPLE_DATABASE_FILE
+			else:
+				TinTin.echo("Error: '{0}' is a directory, not a file.".format(SAMPLE_DATABASE_FILE), "python")
 		else:
-			TinTin.send("%s %s" % (action, door), "mume")
+			path = None
+			TinTin.echo("Error: neither '{0}' nor '{1}' can be found.".format(DATABASE_FILE, SAMPLE_DATABASE_FILE), "mume")
+		if not path:
+			self.rooms = {}
+		else:
+			try:
+				with codecs.open(path, "rb", encoding="utf-8") as data:
+					self.rooms = json.load(data)
+			except IOError as e:
+				self.rooms = {}
+				TinTin.echo("{0}: '{1}'".format(e.strerror, e.filename), "mume")
+			except ValueError as e:
+				self.rooms = {}
+				TinTin.echo("Corrupted file: '{0}'".format(path), "mume")
 
-def add(newDoor="", newDirection=""):
-	global rooms
-	global roomName
-	global validDirections
-	currentRoom = roomName
-	newDoor = newDoor.strip().lower()
-	newDirection = newDirection.strip().lower()
-	newDirection = "".join(dir for dir in validDirections if newDirection and dir.startswith(newDirection))
-	if not currentRoom:
-		return TinTin.echo("Error!  Current room undefined.", "mume")
-	elif not newDoor or not newDirection:
-		return TinTin.echo("Syntax: dadd [door] [%s]" % "|".join(validDirections), "mume")
-	elif currentRoom not in rooms:
-		rooms[currentRoom] = []
-	TinTin.echo("adding the door '%s' located '%s' to '%s'." % (newDoor, newDirection, currentRoom), "mume")
-	rooms[currentRoom].append([newDoor, newDirection])
-	rooms[currentRoom] = list(uniq(sorted(rooms[currentRoom], key=lambda (door, direction): validDirections.index(direction) if direction in validDirections else 0)))
-	save()
+	def uniq(self, lst):
+		last = object()
+		for item in lst:
+			if item != last:
+				yield item
+				last = item
 
-def delete(delDoor="", delDirection=""):
-	global rooms
-	global roomName
-	global validDirections
-	currentRoom = roomName
-	delDoor = delDoor.strip().lower()
-	delDirection = delDirection.strip().lower()
-	delDirection = "".join(dir for dir in validDirections+["all"] if delDirection and dir.startswith(delDirection))
-	if not currentRoom:
-		return TinTin.echo("Error!  Room is undefined.", "mume")
-	elif currentRoom not in rooms:
-		return TinTin.echo("Error! The current room isn't in the database.", "mume")
-	elif not delDoor or not delDirection:
-		return TinTin.echo("Syntax: ddel [door|all] [%s|all]" % "|".join(validDirections), "mume")
-	elif delDoor!="all" and delDirection!="all":
-		if [delDoor, delDirection] in rooms[currentRoom]:
-			TinTin.echo("Deleting '%s' located '%s' from '%s'." % (delDoor, delDirection, currentRoom), "mume")
-			rooms[currentRoom].remove([delDoor, delDirection])
+	def save(self):
+		with codecs.open(DATABASE_FILE, "wb", encoding="utf-8") as data:
+			json.dump(self.rooms, data, sort_keys=True, indent=2, separators=(",", ": "))
+
+	def info(self, roomName="", searchString=""):
+		roomName = roomName.strip().lower()
+		searchString = searchString.strip().lower()
+		results = []
+		if searchString:
+			results.extend([name for name in self.rooms if searchString in name])
+		elif roomName and roomName in self.rooms:
+			searchString = roomName
+			results.append(searchString)
 		else:
-			return TinTin.echo("'%s' does not have any exits to the '%s' with the name '%s'." % (currentRoom, delDirection, delDoor), "mume")
-	elif delDirection != "all":
-		# Check to see if the current room has any secrets in the given direction.
-		if [[door, direction] for door, direction in rooms[currentRoom] if direction == delDirection]:
-			TinTin.echo("Deleting all exits '%s' from '%s'." % (delDirection, currentRoom), "mume")
-			rooms[currentRoom] = [[door, direction] for door, direction in rooms[currentRoom] if direction != delDirection]
+			return TinTin.echo("Error!  Room undefined.", "mume")
+		if not results:
+			TinTin.echo("There aren't any rooms named '{0}' in the database.".format(searchString), "mume")
 		else:
-			return TinTin.echo("'%s' does not have any exits to the '%s'." % (currentRoom, delDirection), "mume")
-	elif delDoor != "all":
-		# Check to see if the current room has any secret doors with the given name.
-		if [[door, direction] for door, direction in rooms[currentRoom] if door == delDoor]:
-			TinTin.echo("Deleting all secret doors with the name '%s' from '%s'." % (delDoor, currentRoom), "mume")
-			rooms[currentRoom] = [[door, direction] for door, direction in rooms[currentRoom] if door != delDoor]
-		else:
-			return TinTin.echo("'%s' does not have any secret doors called '%s'." % (currentRoom, delDoor), "mume")
-	if delDoor=="all" and delDirection=="all" or not rooms[currentRoom]:
-		TinTin.echo("Deleting the room '%s' from the database." % currentRoom, "mume")
-		del rooms[currentRoom]
-	save()
+			for result in sorted(results):
+				exits = []
+				for door, direction in self.rooms[result]:
+					if direction:
+						exits.append("{0} ({1})".format(direction, door))
+					else:
+						exits.append("({0})".format(door))
+				TinTin.echo("{0}: {1}".format(result, ", ".join(exits)), "mume")
+
+	def actionAll(self, roomName="", action=""):
+		roomName = roomName.strip().lower()
+		if not roomName:
+			return TinTin.echo("Error!  Current room undefined.", "mume")
+		elif roomName not in self.rooms:
+			return TinTin.echo("There aren't any rooms named '{0}' in the database.".format(roomName), "mume")
+		elif not action:
+			return TinTin.echo("Error! You must specify an action to perform.", "mume")
+		for door, direction in self.rooms[roomName]:
+			if direction:
+				TinTin.send("{0} {1} {2}".format(action, door, direction), "mume")
+			else:
+				TinTin.send("{0} {1}".format(action, door), "mume")
+
+	def add(self, roomName="", newDoor="", newDirection=""):
+		roomName = roomName.strip().lower()
+		newDoor = newDoor.strip().lower()
+		newDirection = newDirection.strip().lower()
+		# If the direction name is shortened, expand it to its full form.
+		newDirection = "".join(dir for dir in VALID_DIRECTIONS if newDirection and dir.startswith(newDirection))
+		if not roomName:
+			return TinTin.echo("Error!  Current room undefined.", "mume")
+		elif not newDoor or not newDirection:
+			return TinTin.echo("Syntax: dadd [door] [{0}]".format("|".join(VALID_DIRECTIONS)), "mume")
+		elif roomName not in self.rooms:
+			self.rooms[roomName] = []
+		TinTin.echo("adding the door '{0}' located '{1}' to '{2}'.".format(newDoor, newDirection, roomName), "mume")
+		self.rooms[roomName].append([newDoor, newDirection])
+		self.rooms[roomName] = list(self.uniq(sorted(self.rooms[roomName], key=lambda (door, direction): VALID_DIRECTIONS.index(direction) if direction in VALID_DIRECTIONS else 0)))
+		self.save()
+
+	def delete(self, roomName="", delDoor="", delDirection=""):
+		roomName = roomName.strip().lower()
+		delDoor = delDoor.strip().lower()
+		delDirection = delDirection.strip().lower()
+		# If the direction name is shortened, expand it to its full form.
+		delDirection = "".join(dir for dir in VALID_DIRECTIONS + ["all"] if delDirection and dir.startswith(delDirection))
+		if not roomName:
+			return TinTin.echo("Error!  Room is undefined.", "mume")
+		elif roomName not in self.rooms:
+			return TinTin.echo("Error! The current room isn't in the database.", "mume")
+		elif not delDoor or not delDirection:
+			return TinTin.echo("Syntax: ddel [door|all] [{0}|all]".format("|".join(VALID_DIRECTIONS)), "mume")
+		elif delDoor != "all" and delDirection != "all":
+			if [delDoor, delDirection] in self.rooms[roomName]:
+				TinTin.echo("Deleting '{0}' located '{1}' from '{2}'.".format(delDoor, delDirection, roomName), "mume")
+				self.rooms[roomName].remove([delDoor, delDirection])
+			else:
+				return TinTin.echo("'{0}' does not have any exits to the '{1}' with the name '{2}'.".format(roomName, delDirection, delDoor), "mume")
+		elif delDirection != "all":
+			# Check to see if the current room has any secrets in the given direction.
+			if [[door, direction] for door, direction in self.rooms[roomName] if direction == delDirection]:
+				TinTin.echo("Deleting all exits '{0}' from '{1}'.".format(delDirection, roomName), "mume")
+				self.rooms[roomName] = [[door, direction] for door, direction in self.rooms[roomName] if direction != delDirection]
+			else:
+				return TinTin.echo("'{0}' does not have any exits to the '{1}'.".format(roomName, delDirection), "mume")
+		elif delDoor != "all":
+			# Check to see if the current room has any secret doors with the given name.
+			if [[door, direction] for door, direction in self.rooms[roomName] if door == delDoor]:
+				TinTin.echo("Deleting all secret doors with the name '{0}' from '{1}'.".format(delDoor, roomName), "mume")
+				self.rooms[roomName] = [[door, direction] for door, direction in self.rooms[roomName] if door != delDoor]
+			else:
+				return TinTin.echo("'{0}' does not have any secret doors called '{1}'.".format(roomName, delDoor), "mume")
+		if delDoor == "all" and delDirection == "all" or not self.rooms[roomName]:
+			TinTin.echo("Deleting the room '{0}' from the database.".format(roomName), "mume")
+			del self.rooms[roomName]
+		self.save()
