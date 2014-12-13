46a8f83943fd840aacbef647ee7c9a768e60e78d
Nick Stockton <nstockton@users.noreply.github.com>
2014-12-13 07:20:51 -0800

    Sounds script updated to Python 3

--- pyLib/sounds.py
+++ pyLib/sounds.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 import random
 
 from .tintin import TinTin
@@ -9,64 +9,60 @@ except ImportError:
 	TinTin.echo("Unable to import pyGame: please make sure it is installed.", "gts")
 	mixer = None
 
-
-SOUNDS_DIR = "sounds"
-
-sounds = {}
-muted = False
+SOUNDS_DIRECTORY = "sounds"
 
 
-def play(filename="", volume="100"):
-	global sounds
-	filename = filename.strip()
-	if muted or not filename or not mixer:
-		return
-	if not volume.isdigit():
-		return TinTin.echo("Invalid volume: only numbers 1-100 are allowed.", "mume")
-	volume = int(volume) / 100.0
-	path = os.path.join(SOUNDS_DIR, filename)
-	if os.path.isdir(path):
-		filename = random.choice(os.listdir(path))
-		path = os.path.join(path, filename)
-	if not filename in sounds:
-		if os.path.exists(path):
-			sounds[filename] = mixer.Sound(path)
+class Sounds(object):
+	def __init__(self):
+		self.soundObjects = {}
+		self.muted = False
+
+	def play(self, fileName="", volume="100"):
+		fileName = fileName.strip()
+		if self.muted or not fileName or mixer is None:
+			return
+		if not volume.isdigit():
+			return TinTin.echo("Invalid volume: only numbers 1-100 are allowed.", "mume")
+		volume = int(volume) / 100.0
+		path = os.path.join(SOUNDS_DIRECTORY, fileName)
+		if os.path.isdir(path):
+			fileName = random.choice(os.listdir(path))
+			path = os.path.join(path, fileName)
+		if not fileName in self.soundObjects:
+			if os.path.exists(path):
+				self.soundObjects[fileName] = mixer.Sound(path)
+			else:
+				return TinTin.echo("No such sound", "mume")
+		self.soundObjects[fileName].set_volume(volume)
+		self.soundObjects[fileName].play()
+
+	def stop(self, fileName=""):
+		if mixer is None:
+			return
+		fileName = fileName.strip()
+		if fileName:
+			if fileName in self.soundObjects:
+				self.soundObjects[fileName].stop()
+				del self.soundObjects[fileName]
+			else:
+				TinTin.echo("that sound has not been loaded!", "mume")
 		else:
-			return TinTin.echo("No such sound", "mume")
-	sounds[filename].set_volume(volume)
-	sounds[filename].play()
+			for soundObj in self.soundObjects:
+				self.soundObjects[soundObj].stop()
+			self.soundObjects.clear()
 
-def stop(filename=""):
-	global sounds
-	if not mixer:
-		return
-	filename = filename.strip()
-	if filename:
-		if filename in sounds:
-			sounds[filename].stop()
-			del sounds[filename]
-		else:
-			TinTin.echo("that sound has not been loaded!", "mume")
-	else:
-		for sound in sounds:
-			sounds[sound].stop()
-		sounds.clear()
+	def mute(self):
+		if mixer is None:
+			return
+		self.muted = self.muted == False
+		if self.muted:
+			self.stop()
+		TinTin.echo("sound {0}.".format("muted" if self.muted else "unmuted"), "mume")
 
-def mute():
-	global muted
-	if not mixer:
-		return
-	muted = muted==False
-	if muted:
-		stop()
-	TinTin.echo("sound %s." % "muted" if muted else "unmuted", "mume")
+	def load(self):
+		if mixer is not None:
+			mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
 
-def load():
-	if not mixer:
-		return
-	mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
-
-def unload():
-	if not mixer:
-		return
-	mixer.quit()
+	def unload(self):
+		if mixer is not None:
+			mixer.quit()
